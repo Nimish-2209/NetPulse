2f4ebdb5f0e215c9ae683fc32ded941638ce8f1c
Nimish Dhumale <161454567+Nimish-2209@users.noreply.github.com>
2026-06-18 22:32:57 -0400

    changed color

--- apps/api/src/models/Team.js
+++ apps/api/src/models/Team.js
@@ -2,9 +2,10 @@ import mongoose from 'mongoose';
 
 const teamSchema = new mongoose.Schema({
     name: { type: String, required: true, trim: true, minlength: 2, maxlength: 100 },
-    slug: {type: String, required: true, lowercase: true, trim: true, unique: true, index: true },
-    createdBy: {type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }},
+    slug: { type: String, required: true, lowercase: true, trim: true, unique: true, index: true },
+    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }
+},
     { timestamps: true }
 );
-
+abcd
 export const Team = mongoose.model('Team', teamSchema);
